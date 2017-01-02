9849c8270d131d80fd28a1fa65081ee8a1f5b376
Mike Harris <mikeharris.mailbox@gmail.com>
2017-01-01 20:22:17 -0600

    1/1/2017 Commit 2

--- Slingshot_Fuseman/Assets/Slingshot.cs
+++ Slingshot_Fuseman/Assets/Slingshot.cs
@@ -6,12 +6,13 @@ public class Slingshot : MonoBehaviour {
 
 
     public GameObject ballPrefab;
-    public GameObject slingGO;
+    public GameObject slingGO; //slingshot band
     private bool ready = true; //variable to tell us whether our slinghshot is loaded
     private GameObject currBall;
     private SteamVR_TrackedObject trackedController;
     private bool inSlingShot = false;
     private Vector3 slingShotStart; // starting position of slingshot band
+    public float strength = 20f;
 
 	// Use this for initialization
 	void Start () {
@@ -30,6 +31,9 @@ public class Slingshot : MonoBehaviour {
             ready = false; //prevents duplicate balls from getting created
         }
 
+        currBall.transform.LookAt(slingShotStart); //the ball is facing forward. needed for laser arc
+
+
         if (trackedController != null)
         {
             var device = SteamVR_Controller.Input((int)trackedController.index); // tracks controller 
@@ -38,8 +42,17 @@ public class Slingshot : MonoBehaviour {
             {
                 if (device.GetTouchUp(SteamVR_Controller.ButtonMask.Trigger)) //when controller is released, have it reset (touchUp)
                 {
-                    inSlingShot = false; //
-                    slingGO.transform.position = slingShotStart;
+                    ready = true; //spawns another ball since the last one is shot
+                    inSlingShot = false; // controller is no longer selecting the slingshot
+
+                    Vector3 ballPos = currBall.transform.position; //create variable for current position of currBall
+
+                    slingGO.transform.position = slingShotStart; //sets slingshot back to starting position
+                    currBall.transform.parent = null; //this removes the ball from the slingshot so it's no longer attached
+
+                    Rigidbody r = currBall.GetComponent<Rigidbody>(); 
+                    r.velocity = (slingShotStart - ballPos) * strength; //this fires the ball in the direction from where you let go to where the start position is, at velocity of strength
+                    r.useGravity = true; //turns on gravity within rigidbody component of curBall
                 }
 
                 else if (device.GetTouch(SteamVR_Controller.ButtonMask.Trigger)) // if trigger is pressed
